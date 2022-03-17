6cdd6a17b2ba69a1ae081e14903602eae00f2014
shubhamchouhan23 <63260303+shubhamchouhan23@users.noreply.github.com>
2022-03-17 11:58:30 +0530

    Create basic_IO_program.cpp

---
+++ basic_IO_program.cpp
@@ -0,0 +1,18 @@
+/*Basic IO program*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	cout << "This is the first.\n";			//printing output
+	
+	cout << "Enter a No.\n";
+	cin >> i;							//getting input to 'i'
+	
+	cout << i << " squared is " << i*i << endl;
+
+	return 0 ;
+	
+}
